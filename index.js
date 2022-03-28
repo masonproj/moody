93439ff8528f512e5b479ca8e57020d9c821ede3
gustavocsak <78715220+gustavocsak@users.noreply.github.com>
2022-03-28 13:10:02 -0800

    Added information for zenquote keyword endpoint

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.send('index.html');
 });
+
+/*
+
+Zenquotes
+
+6. Request quotes by keyword
+
+To filter quotes by keyword, simply add &keyword=[keyword] to the end of your request.
+For example: https://zenquotes.io/api/quotes/[YOUR_API_KEY]&keyword=happiness would return a maximum of 50 random quotes matching the keyword “happiness”.
+To see a list of keywords currently supported, visit the following link: https://zenquotes.io/keywords
+
+*/
