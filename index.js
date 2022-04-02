5380b41f5ccea1cfbb3006304c5038ea0fdaa3a4
masonproj <irvine.mason@gmail.com>
2022-04-01 18:44:06 -0700

    fixed index

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get('/', function(req, res) {
+    res.render('index');
+})
+
 //GET DATA FROM FORM
 app.post('/submitMood', async function (req, res) {
     let request = {
