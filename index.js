22073a94c3a119f322f71ad2d49d7f49bd4239df
masonproj <irvine.mason@gmail.com>
2022-04-01 17:21:51 -0700

    created submission logic for form

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const fetch = require('node-fetch');
 const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
-const { response } = require('express');
 const app = express();
 
 const API_KEY = process.env.API_KEY;
@@ -19,16 +18,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
-
 //GET DATA FROM FORM
-app.post('/submitMood', function (req, res) {
-    console.log(req.body);
+app.post('/submitMood', async function (req, res) {
+    let request = {
+        mood: req.body.mood,
+        description: req.body.description,
+        quote: req.body.quote,
+        video: req.body.video,
+    };
+
+    if (request.quote) {
+        let result = await fetch('http://localhost:8080/dailyQuote');
+        let quote = await result.text();
+        request.quote = quote;
+    }
+
+    if (request.video) {
+        let result = await fetch('http://localhost:8080/videoTest/' + req.body.mood);
+        let video = await result.text();
+        request.video = video;
+    }
 
     //Just responds with the index again. Can check if there was data in req.body and if true also respond with a confirmation variable or the entry list page
-    res.render('index');
+    res.render('mood', request);
 });
 
 app.get('/randomQuote', async function (req, res) {
@@ -42,29 +54,6 @@ app.get('/login', async function (req, res) {
     res.render('login');
 });
 
-app.get('/mood', async function (req, res) {
-    let dummyRequest = {
-        mood: 'happy',
-        description: 'this is a description',
-        quote: 'true',
-        video: 'true',
-    };
-
-    if (dummyRequest.quote) {
-        let result = await fetch('http://localhost:8080/dailyQuote');
-        let quote = await result.text();
-        dummyRequest.quote = quote;
-    }
-
-    if (dummyRequest.video) {
-        let result = await fetch('http://localhost:8080/videoTest');
-        let video = await result.text();
-        dummyRequest.video = video;
-    }
-
-    res.render('mood', dummyRequest);
-});
-
 app.get('/dailyQuote', async function (req, res) {
     let apiAddress = 'https://zenquotes.io/api/today';
     const response = await fetch(apiAddress);
@@ -74,7 +63,7 @@ app.get('/dailyQuote', async function (req, res) {
 
 app.get('/videoTest/:mood', async function (req, res) {
     let mood = req.params.mood;
-    let apiAddress = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=' + mood + '&key=' + API_KEY;
+    let apiAddress = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=' + mood + '+tips+motivation&key=' + API_KEY;
     const response = await fetch(apiAddress);
     const result = await response.json();
 
